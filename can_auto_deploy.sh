56f8d6aac13669310467e0ca0fce9d4eab65ca24
Gerard Dethier <g.dethier@gmail.com>
2017-02-01 20:15:51 +0100

    Prevent deployment in case of PR.

--- can_auto_deploy.sh
+++ can_auto_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=$(xmlstarlet sel -N "n=http://maven.apache.org/POM/4.0.0" -t -v "/n:project/n:version" pom.xml)
-if [[ $VERSION == *-SNAPSHOT ]]
+if [[ $VERSION == *-SNAPSHOT && "$TRAVIS_PULL_REQUEST" == false ]]
 then
 	exit 0
 else
