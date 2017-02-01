565d5a8ec83b651a9d1e9b68fa3d3db7c10216c5
Gerard Dethier <g.dethier@gmail.com>
2017-02-01 16:58:55 +0100

    Fix archetype generation.

--- pousse-cafe-simple-app/scripts/archetype_mvn.sh
+++ pousse-cafe-simple-app/scripts/archetype_mvn.sh
@@ -14,12 +14,17 @@ read DESCRIPTION <<-"_EOF_"
 Pousse-Café is a framework assisting in writing DDD-based applications. This is a simple meta-app archetype.
 _EOF_
 
-mvn archetype:create-from-project -Darchetype.properties=archetype.properties
+mvn archetype:create-from-project -Darchetype.properties=archetype.properties -Darchetype.keepParent=true
 
 cd target/generated-sources/archetype/
 
 xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -u "/n:project/n:description" -v "$DESCRIPTION" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -i "/n:project/n:build" -t elem -n properties pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:properties" -t elem -n "project.build.sourceEncoding" -v "UTF-8" pom.xml
 
-sed -i s/\$\{project.version\}/$VERSION/g src/main/resources/archetype-resources/pom.xml
+TEMPLATE_POM=src/main/resources/archetype-resources/pom.xml
+sed -i s/\$\{project.version\}/$VERSION/g $TEMPLATE_POM
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:url" $TEMPLATE_POM
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:build" $TEMPLATE_POM
 
 mvn "$@"
