ed143be9070bbd4be1c24c23e00c144cba3e4029
Gerard Dethier <g.dethier@gmail.com>
2017-02-01 20:33:07 +0100

    Set archetype parent POM.

--- pousse-cafe-simple-app/scripts/archetype_mvn.sh
+++ pousse-cafe-simple-app/scripts/archetype_mvn.sh
@@ -21,20 +21,27 @@ mvn archetype:create-from-project -Darchetype.properties=archetype.properties -D
 cd target/generated-sources/archetype/
 
 echo "Archetype POM fixing..."
+echo "	Adding parent element"
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -i "/n:project/n:groupId" -t elem -n parent -v "" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:parent" -t elem -n "groupId" -v "org.pousse-cafe-framework" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:parent" -t elem -n "artifactId" -v "pousse-cafe" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:parent" -t elem -n "version" -v "$VERSION" pom.xml
+echo "	Remove elements conflicting with/overriding parent"
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:groupId" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:version" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:developers" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:licenses" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:scm" pom.xml
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:distributionManagement" pom.xml
 echo "	Updating description"
 xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -u "/n:project/n:description" -v "$DESCRIPTION" pom.xml
-echo "	Adding properties empty element"
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -i "/n:project/n:build" -t elem -n properties -v "" pom.xml
-echo "	Adding source encoding"
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:properties" -t elem -n "project.build.sourceEncoding" -v "UTF-8" pom.xml
 
 echo "Archetype resource POM fixing..."
 TEMPLATE_POM=src/main/resources/archetype-resources/pom.xml
 echo "	Fixing dependencies version"
 sed -i s/\$\{project.version\}/$VERSION/g $TEMPLATE_POM
-echo "	Removing URL"
+echo "	Removing useless elements"
 xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:url" $TEMPLATE_POM
-echo "	Removing build"
 xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:build" $TEMPLATE_POM
 
 mvn "$@"
