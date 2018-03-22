3a9eb6678237fc0bf5ccb6cce2e1f211ac2231b5
Gerard Dethier <g.dethier@gmail.com>
2018-03-22 14:18:30 +0100

    Add default logger in DomainProcess.

--- pousse-cafe-core/src/main/java/poussecafe/process/DomainProcess.java
+++ pousse-cafe-core/src/main/java/poussecafe/process/DomainProcess.java
@@ -1,5 +1,9 @@
 package poussecafe.process;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class DomainProcess extends TransactionAwareService {
 
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 }
