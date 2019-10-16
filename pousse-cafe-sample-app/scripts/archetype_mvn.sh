6a856af2cf8443680b8ced3e20904cb6da8ae27f
Gerard Dethier <g.dethier@gmail.com>
2019-10-16 16:19:56 +0200

    Improve archetype and remove versions backup files.

--- pousse-cafe-sample-app/scripts/archetype_mvn.sh
+++ pousse-cafe-sample-app/scripts/archetype_mvn.sh
@@ -21,40 +21,23 @@ mvn archetype:create-from-project -Darchetype.properties=archetype.properties -D
 cd target/generated-sources/archetype/
 
 echo "Archetype POM fixing..."
+ARCHETYPE_POM=pom.xml
 
-echo "  Adding parent element"
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -i "/n:project/n:groupId" -t elem -n parent -v "" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:parent" -t elem -n "groupId" -v "org.pousse-cafe-framework" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:parent" -t elem -n "artifactId" -v "pousse-cafe" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:parent" -t elem -n "version" -v "$VERSION" pom.xml
+echo "    Updating description"
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -u "/n:project/n:description" -v "$DESCRIPTION" $ARCHETYPE_POM
 
-echo "	Remove useless elements"
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:groupId" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:version" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:developers" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:licenses" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:scm" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:distributionManagement" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:url" pom.xml
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:name" pom.xml
+echo "    Adding properties element"
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -i "/n:project/n:build" -t elem -n properties -v "" $ARCHETYPE_POM
+xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:properties" -t elem -n "project.build.sourceEncoding" -v "UTF-8" $ARCHETYPE_POM
 
-echo "	Updating description"
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -u "/n:project/n:description" -v "$DESCRIPTION" pom.xml
 
 echo "Archetype resource POM fixing..."
 TEMPLATE_POM=src/main/resources/archetype-resources/pom.xml
 
-echo "  Adding properties element"
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -i "/n:project/n:dependencies" -t elem -n properties -v "" $TEMPLATE_POM
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:properties" -t elem -n "maven.compiler.source" -v "1.8" $TEMPLATE_POM
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:properties" -t elem -n "maven.compiler.target" -v "1.8" $TEMPLATE_POM
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -s "/n:project/n:properties" -t elem -n "project.build.sourceEncoding" -v "UTF-8" $TEMPLATE_POM
-
-echo "	Fixing dependencies version"
+echo "    Fixing dependencies version"
 sed -i s/\$\{project.version\}/$VERSION/g $TEMPLATE_POM
 
-echo "	Removing useless elements"
+echo "    Removing useless elements"
 xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:url" $TEMPLATE_POM
-xmlstarlet ed -L -N "n=http://maven.apache.org/POM/4.0.0" -d "/n:project/n:build" $TEMPLATE_POM
 
 mvn "$@"
