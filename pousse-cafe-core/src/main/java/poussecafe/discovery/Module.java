f746de34debc394336b354a250f7b74b9ac1dc06
Gerard Dethier <g.dethier@gmail.com>
2020-02-12 10:33:35 +0100

    Add Module annotation.

---
+++ pousse-cafe-core/src/main/java/poussecafe/discovery/Module.java
@@ -0,0 +1,15 @@
+package poussecafe.discovery;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface Module {
+
+    Class<? extends poussecafe.domain.Module> value();
+}
--- pousse-cafe-doc/src/test/java/poussecafe/sample/test/Test.java
+++ pousse-cafe-doc/src/test/java/poussecafe/sample/test/Test.java
@@ -1,10 +1,12 @@
 package poussecafe.sample.test;
 
+import poussecafe.discovery.Module;
 import poussecafe.domain.Process;
 
 /**
  * This is the description of Test process.
  */
+@Module(Sample.class)
 public interface Test extends Process {
 
 }
