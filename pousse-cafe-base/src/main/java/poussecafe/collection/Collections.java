557cf78af5fc9fc690422754096b00f0ed69a763
Gerard Dethier <g.dethier@gmail.com>
2020-06-18 18:36:22 +0200

    feat: add editors for map and set.

--- pousse-cafe-base/src/main/java/poussecafe/collection/Collections.java
+++ pousse-cafe-base/src/main/java/poussecafe/collection/Collections.java
@@ -2,6 +2,7 @@ package poussecafe.collection;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Collections {
@@ -9,13 +10,23 @@ public class Collections {
     @SafeVarargs
     public static <T> Set<T> asSet(T... items) {
         Set<T> result = new HashSet<>();
-        for (T item : items) {
-            result.add(item);
-        }
+        java.util.Collections.addAll(result, items);
         return result;
     }
 
     public static <V> ListEditor<V> edit(List<V> list) {
         return new ListEditor<>(list);
+    }
+
+    public static <K, V> MapEditor<K, V> edit(Map<K, V> map) {
+        return new MapEditor<>(map);
+    }
+
+    public static <V> SetEditor<V> edit(Set<V> set) {
+        return new SetEditor<>(set);
+    }
+
+    private Collections() {
+
     }
 }
