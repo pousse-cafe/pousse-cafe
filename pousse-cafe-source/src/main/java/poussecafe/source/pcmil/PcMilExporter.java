d7ceab2cd64cc49dde00b2fb35a194f5f192d2b1
Gerard Dethier <g.dethier@gmail.com>
2020-07-03 17:46:28 +0200

    feat: chain listeners in process.
    #150

--- pousse-cafe-source/src/main/java/poussecafe/source/pcmil/PcMilExporter.java
+++ pousse-cafe-source/src/main/java/poussecafe/source/pcmil/PcMilExporter.java
@@ -1,6 +1,8 @@
 package poussecafe.source.pcmil;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -26,33 +28,10 @@ public class PcMilExporter {
     private void generatePcMil() {
         var builder = new StringBuilder();
         while(!listeners.isEmpty()) {
-            Optional<MessageListener> rootListener = findRootListener(listeners);
-            if(rootListener.isEmpty()) {
-                rootListener = Optional.of(listeners.get(0));
-            }
+            MessageListener next = next();
+            appendTopListener(builder, next);
 
-            builder.append(rootListener.orElseThrow().consumedMessageName());
-            builder.append(" -> ");
-
-            if(rootListener.orElseThrow().container().type() == MessageListenerContainerType.FACTORY) {
-                builder.append(rootListener.orElseThrow().container().aggregateName().orElseThrow());
-                builder.append("Factory").append('\n');
-            } else if(rootListener.orElseThrow().container().type() == MessageListenerContainerType.REPOSITORY) {
-                builder.append(rootListener.orElseThrow().container().aggregateName().orElseThrow());
-                builder.append("Repository").append('\n');
-            } else if(rootListener.orElseThrow().container().type() == MessageListenerContainerType.ROOT) {
-                builder.append('[').append(rootListener.orElseThrow().runnerName().orElse("")).append(']').append('\n');
-                builder.append("    @").append(rootListener.orElseThrow().container().aggregateName().orElseThrow())
-                    .append('[').append(rootListener.orElseThrow().methodName()).append(']')
-                    .append(':').append('\n');
-                for(ProducedEvent producedEvent : rootListener.orElseThrow().producedEvents()) {
-                    builder.append("        :").append(producedEvent.eventName()).append('!').append(" -> . [TODO chain]").append('\n');
-                }
-            } else {
-                builder.append(". [external]");
-            }
-
-            listeners.remove(rootListener.orElseThrow());
+            listeners.remove(next);
             if(!listeners.isEmpty()) {
                 builder.append('\n');
             }
@@ -62,13 +41,107 @@ public class PcMilExporter {
 
     private List<MessageListener> listeners;
 
-    private Optional<MessageListener> findRootListener(List<MessageListener> listeners) {
+    private MessageListener next() {
+        Optional<MessageListener> rootListener = findTopListener(listeners);
+        if(rootListener.isEmpty()) {
+            return listeners.get(0);
+        } else {
+            return rootListener.get();
+        }
+    }
+
+    private Optional<MessageListener> findTopListener(List<MessageListener> listeners) {
         return listeners.stream()
                 .filter(listener -> !producedEvents.contains(listener.consumedMessageName()))
                 .findFirst();
     }
 
     private Set<String> producedEvents = new HashSet<>();
+
+    private void appendTopListener(StringBuilder builder, MessageListener rootListener) {
+        appendListeners(0, builder, rootListener.consumedMessageName(), findAndRemoveListeners(rootListener.consumedMessageName()));
+    }
+
+    private void appendListeners(int level, StringBuilder builder, String messageName, List<MessageListener> isteners) {
+        builder.append(messageName);
+
+        if(isteners.isEmpty()) {
+            builder.append(" -> .").append('\n');
+        } else if(isteners.size() == 1) {
+            builder.append(" -> ");
+        } else {
+            builder.append(" -> ").append('\n');
+        }
+
+        for(MessageListener listener : isteners) {
+            if(isteners.size() > 1) {
+                builder.append(indent(level + 1)).append(" -> ");
+            }
+            appendListener(level, builder, listener);
+        }
+    }
+
+    private void appendListener(int level, StringBuilder builder, MessageListener listener) {
+        if(listener.container().type() == MessageListenerContainerType.FACTORY) {
+            appendFactoryListener(level, builder, listener);
+        } else if(listener.container().type() == MessageListenerContainerType.REPOSITORY) {
+            appendRepositoryListener(level, builder, listener);
+        } else if(listener.container().type() == MessageListenerContainerType.ROOT) {
+            appendAggregateRootListener(level, builder, listener);
+        } else {
+            builder.append(". [external]");
+        }
+    }
+
+    private void appendFactoryListener(int level, StringBuilder builder, MessageListener listener) {
+        builder.append(listener.container().aggregateName().orElseThrow());
+        builder.append("Factory").append('\n');
+        for(ProducedEvent producedEvent : listener.producedEvents()) {
+            appendListeners(level, builder, producedEvent.eventName() + "!", findAndRemoveListeners(producedEvent.eventName()));
+        }
+    }
+
+    private void appendRepositoryListener(int level, StringBuilder builder, MessageListener listener) {
+        builder.append(listener.container().aggregateName().orElseThrow());
+        builder.append("Repository").append('\n');
+        for(ProducedEvent producedEvent : listener.producedEvents()) {
+            appendListeners(level, builder, producedEvent.eventName() + "!", findAndRemoveListeners(producedEvent.eventName()));
+        }
+    }
+
+    private void appendAggregateRootListener(int level, StringBuilder builder, MessageListener listener) {
+        builder.append('[').append(listener.runnerName().orElse("")).append(']').append('\n');
+        builder.append(indent(level + 1)).append("@").append(listener.container().aggregateName().orElseThrow())
+            .append('[').append(listener.methodName()).append(']')
+            .append(':').append('\n');
+        for(ProducedEvent producedEvent : listener.producedEvents()) {
+            builder.append(indent(level + 2)).append(':');
+            appendListeners(level + 2, builder, producedEvent.eventName() + "!", findAndRemoveListeners(producedEvent.eventName()));
+        }
+    }
+
+    private String indent(int level) {
+        var builder = new StringBuilder();
+        for(int i = 0; i < level; ++i) {
+            builder.append(TAB);
+        }
+        return builder.toString();
+    }
+
+    private static final Object TAB = "    ";
+
+    private List<MessageListener> findAndRemoveListeners(String eventName) {
+        var removedListeners = new ArrayList<MessageListener>();
+        Iterator<MessageListener> iterator = listeners.iterator();
+        while(iterator.hasNext()) {
+            MessageListener listener = iterator.next();
+            if(listener.consumedMessageName().equals(eventName)) {
+                removedListeners.add(listener);
+                iterator.remove();
+            }
+        }
+        return removedListeners;
+    }
 
     public static class Builder {
 
