69961de877e0319e3f0f9dc9768e4eac2a56d3c9
Gerard Dethier <g.dethier@gmail.com>
2021-03-08 09:59:42 +0100

    feat: documented auto-adapter interface.
    #217

--- pousse-cafe-attribute/src/main/java/poussecafe/attribute/AutoAdapter.java
+++ pousse-cafe-attribute/src/main/java/poussecafe/attribute/AutoAdapter.java
@@ -1,5 +1,18 @@
 package poussecafe.attribute;
 
+/**
+ * <p>Auto-adapters are used to combine in a single place the data representation of the adapted type and conversion
+ * logic.</p>
+ *
+ * <p>An auto-adapter has at least 2 methods:</p>
+ *
+ * <ul>
+ * <li>a static method <code>adapt(T)</code> returning an instantiate of the auto-adapter given an instance of the adapted type,</li>
+ * <li>an instance method <code>adapt()</code> returning an instance of the adapted type.</li>
+ * </ul>
+ *
+ * @param <T> the adapted type
+ */
 public interface AutoAdapter<T> {
 
     T adapt();
