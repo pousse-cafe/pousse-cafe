d12d1f29f171d72f8914b7291c19aa659f1dad93
Gérard Dethier <info@gerarddethier.be>
2025-02-05 20:52:21 +0100

    feat: move in some core annotations
    pousse-cafe/pousse-cafe-shop-app#8

---
+++ pousse-cafe-core/src/main/java/poussecafe/annotations/ShortDescription.java
@@ -0,0 +1,11 @@
+package poussecafe.annotations;
+
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+@Retention(SOURCE)
+public @interface ShortDescription {
+
+    String value();
+}
---
+++ pousse-cafe-core/src/main/java/poussecafe/annotations/Trivial.java
@@ -0,0 +1,10 @@
+package poussecafe.annotations;
+
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+@Retention(SOURCE)
+public @interface Trivial {
+
+}
